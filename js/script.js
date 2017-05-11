4ce578ddf41c2e323230fde5488df6b2853009a3
Verta-master <vertyagina@gmail.com>
2017-05-11 19:03:05 +0600

    Финальная проверка
    Финальная проверка

--- js/script.js
+++ js/script.js
@@ -9,34 +9,34 @@ var storage = localStorage.getItem("login");
 
 link.addEventListener("click", function (event) {
   event.preventDefault();
-	overlay.classList.add("modal-overlay-show");
-	popup.classList.add("feedback-modal-show");
-	if (storage) {
-		login.value = storage;
-		email.focus();
-	} else {
-		login.focus();
-	}
+  overlay.classList.add("modal-overlay-show");
+  popup.classList.add("feedback-modal-show");
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
 });
-	  
+
 close.addEventListener("click", function (event) {
   event.preventDefault();
   popup.classList.remove("feedback-modal-show");
-	overlay.classList.remove("modal-overlay-show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 form.addEventListener("submit", function (event) {
-	if (!login.value || !email.value) {
-		event.preventDefault();
-	} else {
-		localStorage.setItem("login", login.value);
-	}
+  if (!login.value || !email.value) {
+    event.preventDefault();
+  } else {
+    localStorage.setItem("login", login.value);
+  }
 });
 
 window.addEventListener("keydown", function (event) {
-	if (event.keyCode === 27) {
-		if (popup.classList.contains("feedback-modal-show")) {
-			popup.classList.remove("feedback-modal-show");
-		}
-	}
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("feedback-modal-show")) {
+      popup.classList.remove("feedback-modal-show");
+    }
+  }
 });
